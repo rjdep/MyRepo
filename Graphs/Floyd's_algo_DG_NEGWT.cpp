0aa911f3dc1dee02bb1c8e48451f4b403bd02004
rjdep <89678449+rjdep@users.noreply.github.com>
2023-01-20 16:03:33 +0530

    Create Floyd's_algo_DG_NEGWT.cpp

---
+++ Graphs/Floyd's_algo_DG_NEGWT.cpp
@@ -0,0 +1,24 @@
+int floydWarshall(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    // Write your code here.
+    vector<vector<int>> dist(n+1,vector<int> (n+1,1e9));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==j) dist[i][j]=0;
+        }
+    }
+    for(auto x: edges){
+        dist[x[0]][x[1]]=x[2];
+//         dist[x[1]][x[0]]=x[2];
+    }
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(dist[i][k] != 1e9 && dist[k][j] != 1e9 && dist[i][j]>dist[i][k]+dist[k][j]){
+                    dist[i][j]=dist[i][k]+dist[k][j];
+                }
+            }
+        }
+    }
+    
+    return dist[src][dest];
+}
